69362c9b97155eef1b59346da3857f1abf9a62b9
Chelsea Davis <chelseabrekken@hotmail.com>
2015-04-22 10:29:22 -0700

    Added Game.java, which opens a window for the game to be played in and accepts keyboard input. Also added lines to Engine.java to run game. We can add specifications for "New Game" vs "Load Game" later on. :)

--- Game Files/Engine.java
+++ Game Files/Engine.java
@@ -5,5 +5,8 @@ public class Engine {
     public static void main(String[] args) {
         GameMenu menu = new GameMenu();
         menu.displayMenu();
+        
+        Game g = new Game();
+        g.update();
     }
 }
---
+++ Game Files/Game.java
@@ -0,0 +1,259 @@
+/**
+ * Created by Chelsea Davis on 4/21/2015.
+ */
+
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.KeyAdapter;
+import java.awt.event.KeyEvent;
+import java.awt.event.WindowAdapter;
+import java.awt.event.WindowEvent;
+import java.awt.image.BufferStrategy;
+
+/* Will be needed later.
+import java.awt.Canvas;
+import java.awt.Color;
+import java.awt.Dimension;
+import java.awt.Graphics2D;
+import java.awt.event.KeyAdapter;
+import java.awt.event.KeyEvent;
+import java.awt.event.WindowAdapter;
+import java.awt.event.WindowEvent;
+import java.awt.image.BufferStrategy;
+import java.util.ArrayList;
+
+import javax.swing.JFrame;
+import javax.swing.JPanel;*/
+
+public class Game extends Canvas
+{
+   /** The strategy that allows us to use accelerate page flipping */
+	private BufferStrategy strategy;
+	/** True if the game is currently "running", i.e. the game loop is looping */
+	private boolean gameRunning = true;
+   
+   /** The message to display which waiting for a key press */
+	private String message = "";
+	/** True if we're holding up game play until a key has been pressed */
+	private boolean waitingForKeyPress = true;
+	/** True if the up cursor key is currently pressed */
+	private boolean upPressed = false;
+	/** True if the down cursor key is currently pressed */
+	private boolean downPressed = false;
+	/** True if the left cursor key is currently pressed */
+	private boolean leftPressed = false;
+	/** True if the right cursor key is currently pressed */
+	private boolean rightPressed = false;
+	/** True if space is being pressed */
+	private boolean spacePressed = false;
+	/** True if game logic needs to be applied this loop, normally as a result of a game event */
+	private boolean logicRequiredThisLoop = false;
+
+   /**
+	 * Construct our game and set it running.
+	 */
+	public Game() {
+		// create a frame to contain our game
+		JFrame container = new JFrame("Cutlery Knights");
+		
+		// get hold the content of the frame and set up the resolution of the game
+		JPanel panel = (JPanel) container.getContentPane();
+		panel.setPreferredSize(new Dimension(800,600));
+		panel.setLayout(null);
+		
+		// setup our canvas size and put it into the content of the frame
+		setBounds(0,0,800,600);
+		panel.add(this);
+		
+		// Tell AWT not to bother repainting our canvas since we're
+		// going to do that our self in accelerated mode
+		setIgnoreRepaint(true);
+		
+		// finally make the window visible 
+		container.pack();
+		container.setResizable(false);
+		container.setVisible(true);
+		
+		// add a listener to respond to the user closing the window. If they
+		// do we'd like to exit the game
+		container.addWindowListener(new WindowAdapter() {
+			public void windowClosing(WindowEvent e) {
+				System.exit(0);
+			}
+		});
+		
+		// add a key input system (defined below) to our canvas so we can respond to key pressed
+		addKeyListener(new KeyInputHandler());
+		
+		// request the focus so key events come to us
+		requestFocus();
+
+		// create the buffering strategy which will allow AWT
+		// to manage our accelerated graphics
+		createBufferStrategy(2);
+		strategy = getBufferStrategy();
+		
+		// initialise the entities in our game so there's something
+		// to see at startup
+		//initEntities();
+	}
+	
+	/**
+	 * Start a fresh game, this should clear out any old data and
+	 * create a new set.
+	 */
+	private void startGame() {
+		// clear out any existing entities and intialise a new set
+		//entities.clear();
+		//initEntities();
+		
+		// blank out any keyboard settings we might currently have
+		leftPressed = false;
+		rightPressed = false;
+		spacePressed = false;
+	}
+   
+   
+   //////////////
+   
+   public void update() {
+		long lastLoopTime = System.currentTimeMillis();
+	   System.out.println("Running game!");
+		
+		// keep looping round til the game ends
+		while (gameRunning) {
+
+			//System.out.println("Running game!");
+
+         Graphics2D g = (Graphics2D) strategy.getDrawGraphics();
+			g.setColor(Color.black);
+			g.fillRect(0,0,800,600);
+	
+			// if we're waiting for an "any key" press then draw the 
+			// current message 
+			if (waitingForKeyPress) {
+				g.setColor(Color.white);
+				g.drawString(message,(800-g.getFontMetrics().stringWidth(message))/2,250);
+				g.drawString("Press any key",(800-g.getFontMetrics().stringWidth("Press any key"))/2,300);
+			}
+						
+			// finally pause for a bit. Note: this should run us at about
+			// 100 fps but on windows this might vary each loop due to
+			// a bad implementation of timer
+			try { Thread.sleep(10); } catch (Exception e) {}
+		}
+	}
+
+   
+   
+   
+   //////////////////////////
+   
+   
+   
+   
+   private class KeyInputHandler extends KeyAdapter {
+		/** The number of key presses we've had while waiting for an "any key" press */
+		private int pressCount = 1;
+		
+		/**
+		 * Notification from AWT that a key has been pressed. Note that
+		 * a key being pressed is equal to being pushed down but *NOT*
+		 * released. Thats where keyTyped() comes in.
+		 *
+		 * @param e The details of the key that was pressed 
+		 */
+		public void keyPressed(KeyEvent e) {
+			// if we're waiting for an "any key" typed then we don't 
+			// want to do anything with just a "press"
+			if (waitingForKeyPress) {
+				return;
+			}
+
+
+			if (e.getKeyCode() == KeyEvent.VK_UP) {
+				System.out.println("Pressed up key!");
+				upPressed = true;
+			}
+			if (e.getKeyCode() == KeyEvent.VK_DOWN) {
+				System.out.println("Pressed down key!");
+				downPressed = true;
+			}
+			if (e.getKeyCode() == KeyEvent.VK_LEFT) {
+				System.out.println("Pressed left key!");
+				leftPressed = true;
+			}
+			if (e.getKeyCode() == KeyEvent.VK_RIGHT) {
+				System.out.println("Pressed right key!");
+				rightPressed = true;
+			}
+			if (e.getKeyCode() == KeyEvent.VK_SPACE) {
+				System.out.println("Pressed space key!");
+				spacePressed = true;
+			}
+		} 
+		
+		/**
+		 * Notification from AWT that a key has been released.
+		 *
+		 * @param e The details of the key that was released 
+		 */
+		public void keyReleased(KeyEvent e) {
+			// if we're waiting for an "any key" typed then we don't 
+			// want to do anything with just a "released"
+			if (waitingForKeyPress) {
+				return;
+			}
+
+			if (e.getKeyCode() == KeyEvent.VK_UP) {
+				upPressed = false;
+			}
+			if (e.getKeyCode() == KeyEvent.VK_DOWN) {
+				downPressed = false;
+			}
+			if (e.getKeyCode() == KeyEvent.VK_LEFT) {
+				leftPressed = false;
+			}
+			if (e.getKeyCode() == KeyEvent.VK_RIGHT) {
+				rightPressed = false;
+			}
+			if (e.getKeyCode() == KeyEvent.VK_SPACE) {
+				spacePressed = false;
+			}
+		}
+
+		/**
+		 * Notification from AWT that a key has been typed. Note that
+		 * typing a key means to both press and then release it.
+		 *
+		 * @param e The details of the key that was typed. 
+		 */
+		public void keyTyped(KeyEvent e) {
+			// if we're waiting for a "any key" type then
+			// check if we've recieved any recently. We may
+			// have had a keyType() event from the user releasing
+			// the shoot or move keys, hence the use of the "pressCount"
+			// counter.
+			if (waitingForKeyPress) {
+				if (pressCount == 1) {
+					// since we've now recieved our key typed
+					// event we can mark it as such and start 
+					// our new game
+					waitingForKeyPress = false;
+					startGame();
+					pressCount = 0;
+				} else {
+					pressCount++;
+				}
+			}
+			
+			// if we hit escape, then quit the game
+			if (e.getKeyChar() == 27) {
+				System.out.println("Hit esc key, exiting game... ");
+				System.exit(0);
+			}
+		}
+	}
+
+
+}
