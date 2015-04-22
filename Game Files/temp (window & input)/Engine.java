3fbf32f2f12150a05b122ebab09078b6e4a23acd
Chelsea Davis <chelseabrekken@hotmail.com>
2015-04-22 10:36:14 -0700

    Created temporary folder for working game window w/ input

---
+++ Game Files/temp (window & input)/Engine.java
@@ -0,0 +1,12 @@
+/**
+ * Created by CountryGeek1 on 4/12/2015.
+ */
+public class Engine {
+    public static void main(String[] args) {
+        GameMenu menu = new GameMenu();
+        menu.displayMenu();
+        
+        Game g = new Game();
+        g.update();
+    }
+}
