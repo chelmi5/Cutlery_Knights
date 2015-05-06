1d36d941dff68b9636258c19b2e2e2a2908a71b3
Chelsea Davis <chelseabrekken@hotmail.com>
2015-05-06 11:01:51 -0700

    Added Character Sprite, Exploration State, and Game Frame class. Will work on integrating with already existing classes.

---
+++ Game Files/CutleryKnight/src/Engine/rtype/CharacterSprite.java
@@ -0,0 +1,83 @@
+package rtype;
+
+import java.awt.Image;
+import java.awt.event.KeyEvent;
+
+import javax.swing.ImageIcon;
+
+public class CharacterSprite {
+
+    private String spriteName = "craft.png";
+
+    private int dx;
+    private int dy;
+    private int x;
+    private int y;
+    private Image image;
+
+    public CharacterSprite() {
+        ImageIcon ii = new ImageIcon(this.getClass().getResource(spriteName));
+        image = ii.getImage();
+        x = 40;
+        y = 60;
+    }
+
+
+    public void move(){
+         x += dx;
+         y += dy;
+    }
+
+    public int getX() {
+        return x;
+    }
+
+    public int getY() {
+        return y;
+    }
+
+    public Image getImage() {
+        return image;
+    }
+
+    public void keyPressed(KeyEvent e) {
+
+        int key = e.getKeyCode();
+
+        if (key == KeyEvent.VK_LEFT) {
+            dx = -3;
+        }
+
+        if (key == KeyEvent.VK_RIGHT) {
+            dx = 3;
+        }
+
+        if (key == KeyEvent.VK_UP) {
+            dy = -3;
+        }
+
+        if (key == KeyEvent.VK_DOWN) {
+            dy = 3;
+        }
+    }
+
+    public void keyReleased(KeyEvent e) {
+        int key = e.getKeyCode();
+
+        if (key == KeyEvent.VK_LEFT) {
+            dx = 0;
+        }
+
+        if (key == KeyEvent.VK_RIGHT) {
+            dx = 0;
+        }
+
+        if (key == KeyEvent.VK_UP) {
+            dy = 0;
+        }
+
+        if (key == KeyEvent.VK_DOWN) {
+            dy = 0;
+        }
+    }
+}
