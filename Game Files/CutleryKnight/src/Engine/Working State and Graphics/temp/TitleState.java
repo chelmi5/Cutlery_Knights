a83a3fd1ed9f8eb8d4206b26ead9e7f70d5f896e
Chelsea Davis <chelseabrekken@hotmail.com>
2015-05-14 16:45:23 -0700

    Working state AND graphics! Added them to a new folder so we can merge them with the other files later.

---
+++ Game Files/CutleryKnight/src/Engine/Working State and Graphics/temp/TitleState.java
@@ -0,0 +1,44 @@
+package temp;
+
+public class TitleState implements IState
+{
+   transient StateMachine stateMachine;
+   private String type;
+   
+   public TitleState(StateMachine s)
+   {
+      this.stateMachine = s;
+      this.type = "title";
+   }
+   
+   public String getType()
+   {
+      return this.type;
+   }
+   
+    public void Update()
+    {
+        // TitleState Update
+        System.out.println("Title State update");
+    }
+ 
+    public void Render()
+    {
+        // TitleState Render
+        System.out.println("Title State Render");
+        //new GameFrame(this.type);
+    }
+ 
+    public void OnEnter()
+    {
+        // TitleState OnEnter
+        System.out.println("Title State Entered");
+        //this.Render();
+    }
+ 
+    public void OnExit()
+    {
+        // TitleState OnExit
+        System.out.println("Title State OnExit");
+    }
+}
