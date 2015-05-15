a04c2ca47f8cf7343f6397dd9b8f2a554705f0fd
countrynerd <reisenauer.james@gmail.com>
2015-05-15 11:38:06 -0700

    Added inventory

--- Game Files/CutleryKnight/src/Objects/Inventory.java
+++ Game Files/CutleryKnight/src/Objects/Inventory.java
@@ -1,8 +1,26 @@
 package Objects;
 
+import java.util.ArrayList;
+
 /**
  * Created by CountryGeek1 on 5/6/2015.
  */
 public class Inventory {
+    ArrayList<Item> mInventory = new ArrayList<Item>();
 
+    public void displayInvitory()
+    {
+        System.out.println("Inventory contains:");
+        for (int i = 0; i < mInventory.size(); i++){
+            System.out.println(i + ": " + mInventory.get(i));
+        }
+    }
+
+    public void addItem(Item item) {
+        mInventory.add(item);
+    }
+
+    public void removeItem(Item item) {
+        mInventory.remove(item);
+    }
 }
--- Game Files/CutleryKnight/src/Objects/Item.java
+++ Game Files/CutleryKnight/src/Objects/Item.java
@@ -4,6 +4,5 @@ package Objects;
  * Created by CountryGeek1 on 5/6/2015.
  */
 public interface Item {
-    abstract void displayName();
-
+    void displayName();
 }
